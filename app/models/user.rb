c14da3f14619112c73f55bab17bd43e231632025
Mike Nicholaides <mike.nicholaides@gmail.com>
2014-04-20 08:49:42 -0400

    Fix inability to update account details

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :trackable, :validatable
   attr_accessor :login
-  
-  #->Prelang (user_login:devise/username_login_support)
+
   has_many :podcasts
   def self.find_first_by_auth_conditions(warden_conditions)
     conditions = warden_conditions.dup
@@ -15,7 +14,6 @@ class User < ActiveRecord::Base
       where(conditions).first
     end
   end
-
 
   devise authentication_keys: [:login]
 end
